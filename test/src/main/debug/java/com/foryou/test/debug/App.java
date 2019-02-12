04aee7c349bbd44d2156df3f9fa57b1aa2050625
liyawei <liyawei@foryou56.com>
2019-02-12 12:10:39 +0800

    test

---
+++ test/src/main/debug/java/com/foryou/test/debug/App.java
@@ -0,0 +1,12 @@
+package com.foryou.test.debug;
+
+import android.app.Application;
+
+/**
+ * Description:
+ * Created by liyawei
+ * Date:2019/1/31 5:33 PM
+ * Email:liyawei@foryou56.com
+ */
+public class App extends Application {
+}
---
+++ test/src/main/debug/java/com/foryou/test/debug/TestActivity.java
@@ -0,0 +1,21 @@
+package com.foryou.test.debug;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.foryou.test.R;
+
+/**
+ * Description:
+ * Created by liyawei
+ * Date:2019/1/31 5:34 PM
+ * Email:liyawei@foryou56.com
+ */
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_activity);
+    }
+}
